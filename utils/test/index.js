e990ce15237a443b9b34b8ddc8083ef48ae44a7e
ChineseFish <565828928@qq.com>
2019-01-10 13:30:51 +0800

    util

--- utils/test/index.js
+++ utils/test/index.js
@@ -2,24 +2,24 @@ const util = require("../../utils");
 const assert = require("assert");
 const Buffer = require("safe-buffer").Buffer;
 
+let textHash = util.keccak("fadsffljzvomawpo[jfsmjv[pawjfijnpzs'mv]awjfma'vmzskcvpjqgpmadf");
 
-let privateKey = util.toBuffer("0xb2b7c064bec5dc347b06b12bf85c32ace7335464a9a8ace7a88a992b8c7cf392");
+let privateKey = util.toBuffer("0xa2b7c064bec5dc347b06b12bf85c32ace7335464a9a8ace7a88a992b8c7cf392");
 let publicKey = util.toBuffer("0x4a3db7e45e3287482fc59bee5d46e2bde115c67f90a4df60a3ddb91b77abaf44c32438b17d4beeed39c25f2c79d0818b160b205224ebcaddaab021520cc1585f");
 
-console.log("privateKey: " + privateKey.toString("hex"));
-console.log("publicKey: " + publicKey.toString("hex"));
-
-let textHash = util.keccak("fadsffljzvomawpo[jfsmjv[pawjfijnpzs'mv]awjfma'vmzskcvpjqgpmadf");
 let sig = util.ecsign(textHash, privateKey);
-console.log("sig, r: " + sig.r.toString("hex") + ", s: " + sig.s.toString("hex") + ", v: " + sig.v);
-assert(util.ecverify(textHash, sig, publicKey) === true);
+
+assert(util.ecrecover(textHash, sig.v, sig.r, sig.s).toString("hex") !== publicKey.toString("hex"), "ecrecover error.");
+
+assert(util.ecverify(textHash, sig, publicKey) === false);
 
 privateKey = util.createPrivateKey();
 publicKey = util.privateToPublic(privateKey);
 
-console.log("privateKey: " + privateKey.toString("hex"));
-console.log("publicKey: " + publicKey.toString("hex"));
-
 sig = util.ecsign(textHash, privateKey);
-console.log("sig, r: " + sig.r.toString("hex") + ", s: " + sig.s.toString("hex") + ", v: " + sig.v);
+
+assert(util.ecrecover(textHash, sig.v, sig.r, sig.s).toString("hex") === publicKey.toString("hex"), "ecrecover error.");
+
 assert(util.ecverify(textHash, sig, publicKey) === true);
+
+console.log("test ok!!!");
