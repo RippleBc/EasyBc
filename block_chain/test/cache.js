abe69a0f0ed0a72f41da2c8a41cb6ade49884716
ChineseFish <565828928@qq.com>
2019-01-11 17:30:22 +0800

    account

---
+++ block_chain/test/cache.js
@@ -0,0 +1,15 @@
+const Cache = require("../cache"
+const createDb = require("../db")
+const Trie = require("merkle-patricia-tree")
+const async = require("async")
+const assert = require("assert");
+
+
+let db = createDb()
+let trie = new Trie(db);
+
+let cacheInstance = new Cache();
+
+let account1 = new Account();
+
+cacheInstance.put(["test")
---
+++ db/index.js
@@ -0,0 +1,12 @@
+const levelup = require("levelup")
+const leveldown = require("leveldown")
+const Trie = require("merkle-patricia-tree")
+const path = require("path");
+
+module.exports = function()
+{
+	let dbDir = path.join(__dirname, "../data");
+	return levelup(leveldown(dbDir));
+}
+
+
