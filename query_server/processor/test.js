a446d8d1ac2165af43b16826b8ec186a24aaa2e4
walker <565828928@qq.com>
2019-03-29 16:06:52 +0800

    query

--- query_server/processor/test.js
+++
@@ -1,9 +0,0 @@
-const initDb = require("../../db")
-const Trie = require("merkle-patricia-tree/secure.js")
-const util = require("../../utils")
-
-// init block chain
-let db = initDb();
-let trie = new Trie(db);
-
-console.log(util.baToHexString(trie.root))
