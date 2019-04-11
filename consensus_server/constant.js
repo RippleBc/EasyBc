aed845c15fdf7c93733aa6d55ddcc3893e8d1f5d
walker <565828928@qq.com>
2019-04-11 14:59:41 +0800

    a

--- consensus_server/constant.js
+++ consensus_server/constant.js
@@ -1,8 +1,8 @@
 module.exports.TRANSACTION_CACHE_MAX_NUM = 500;
 
 //
-module.exports.STAGE_PRIMARY_TIMEOUT = 500;
-module.exports.STAGE_FINISH_TIMEOUT = 500;
+module.exports.STAGE_PRIMARY_TIMEOUT = 2000;
+module.exports.STAGE_FINISH_TIMEOUT = 1000;
 
 //
 module.exports.STAGE_MAX_FINISH_TIMES = 3;
