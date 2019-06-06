d43aa68f698883e5cbd20cd0902afb5009298caa
walker <565828928@qq.com>
2019-06-06 17:17:34 +0800

    a

---
+++ consensus_full_server/test/transactions.js
@@ -0,0 +1,45 @@
+const { assert, expect, should } = require("chai"); 
+const utils = require("../../depends/utils")
+const Transaction = require("../../depends/transaction")
+const Candidate = require("../ripple/data/candidate");
+
+const Buffer = utils.Buffer;
+const rlp = utils.rlp;
+
+const FROM = Buffer.from("21d21b68ded27ce2ef619651d382892c1f77baa4", "hex")
+const TO = Buffer.from("37faf6b0dd1c4faa396f975ffd350e25e8036bc7", "hex")
+const PRIVATE_KEY = Buffer.from("7d4d01cae0cc51fcae5aff8b108e5fd6de5fdd024185f6569cd5c50f07deb493", "hex")
+
+// init a transaction
+const tx = new Transaction({
+	from: FROM,
+	to: TO,
+	value: Buffer.from("10", "hex"),
+	nonce: Buffer.from("01", "hex")
+})
+tx.sign(PRIVATE_KEY)
+const transactionRaw = tx.serialize().toString("hex")
+
+describe("test transactions speed", () => {
+	it("check pack transactions", done => {
+		console.time("pack transactions")
+
+		// init transactions
+		let transactionRawSet = new Array(100).fill(transactionRaw)
+
+		// transfer to Buffer
+		transactionRawSet = transactionRawSet.map(transactionRaw => {
+			return Buffer.from(transactionRaw, "hex");
+		})
+			
+		// init candidate
+		const candidate = new Candidate({
+			transactions: rlp.encode(transactionRawSet)
+		});
+		candidate.sign(PRIVATE_KEY);
+
+		console.timeEnd("pack transactions")
+
+		done();
+	})
+})
