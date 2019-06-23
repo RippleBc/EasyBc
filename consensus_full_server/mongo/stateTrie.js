e8858c087b906c91905827a9eab297fe9f6aec91
ChineseFish <565828928@qq.com>
2019-06-23 20:14:18 +0800

    add mongo

---
+++ consensus_full_server/mongo/stateTrie.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const BlogPost = new Schema({
+  author: ObjectId,
+  title: String,
+  body: String,
+  date: Date
+});
