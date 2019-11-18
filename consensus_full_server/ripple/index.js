6ec3be45a36bb4018a3d76b6af3fcc8a95b1cc09
ChineseFish <565828928@qq.com>
2019-11-18 09:16:33 +0800

    update leader change step length

--- consensus_full_server/ripple/index.js
+++ consensus_full_server/ripple/index.js
@@ -54,7 +54,7 @@ const logger = process[Symbol.for("loggerConsensus")];
 const mysql = process[Symbol.for("mysql")];
 const unlManager = process[Symbol.for("unlManager")];
 
-const WATER_LINE_STEP_LENGTH = 5000;
+const WATER_LINE_STEP_LENGTH = 200;
 const SYSTEM_LOOP_DELAY_TIME = 20;
 
 const SEQUENCE_MODE_MATCH = 1;
