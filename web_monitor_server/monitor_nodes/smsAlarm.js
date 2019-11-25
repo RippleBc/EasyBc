b3b61f207f138fc981e3376ca60df5345a891d1d
ChineseFish <565828928@qq.com>
2019-11-25 16:41:03 +0800

    add smsAlarm module

---
+++ web_monitor_server/monitor_nodes/smsAlarm.js
@@ -0,0 +1,40 @@
+// 必须引入crypto
+const crypto = require('crypto');
+const rp = require("request-promise");
+
+const SMS_USERID = "100020";
+const SMS_APIKEY = "bdfd413e43a443b48046e8344b05d7ca";
+
+class SMSAlarm {
+    constructor() {
+
+    }
+
+    /**
+     * @param {String} mobile
+     * @param {String} content
+     */
+    sendSms({mobile = "18605807725", content}) {
+        const ts = Date.now();
+        const sign = crypto.createHash('md5').update(`${SMS_USERID}${ts}${SMS_APIKEY}`).digest('hex');
+
+        const url = `http://118.31.170.102:8081/api/sms/send?userid=${SMS_USERID}&ts=${ts}&sign=${sign}&mobile=${mobile}&msgcontent=${content}&time=&extnum=`;
+
+        rp({
+            method: "GET",
+            uri: `${url}/getAccountInfo`
+        }).then(response => {
+            console.log(response)
+        }).catch(e => {
+            console.log(e);
+        });
+    }
+
+}
+
+module.exports = SMSAlarm;
+
+
+const testInstantce = new SMSAlarm();
+
+testInstantce.sendSms({content: "123456789"});
