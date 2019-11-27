4299b6439f3a73de2e1d70007383bc199d792c68
ChineseFish <565828928@qq.com>
2019-11-27 08:44:58 +0800

    delete p2pProxy eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,25 +20,6 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
-  }, {
-    name: `p2pProxyServer${processIndex}`,
-    script: 'p2p_proxy_server/index.js',
-    args: '',
-    node_args: '--abort-on-uncaught-exception',
-    instances: 1,
-    autorestart: true,
-    exp_backoff_restart_delay: 1000,
-    watch: false,
-    max_memory_restart: '1G',
-    merge_logs: true,
-    output: '/dev/null',
-    error: '/dev/null',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
   },{
     name: `lightConsensus${processIndex}`,
     script: 'consensus_light_server/index.js',
