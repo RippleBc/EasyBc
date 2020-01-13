dc36769f3a011a94cf5c61970acc3b73ff7d22db
walker <565828928@qq.com>
2020-01-13 16:24:27 +0800

    add config change

--- toolkit/config/index.js
+++ toolkit/config/index.js
@@ -1,10 +1,37 @@
 const switchConfig = require("./switchConfig");
+const utils = require("../../depends/utils");
+
+const createPrivateKey = utils.createPrivateKey;
+const publicToAddress = utils.publicToAddress;
+const privateToPublic = utils.privateToPublic;
+
+const generateKeyPiar = function () {
+    const privateKey = createPrivateKey();
+
+    const publicKey = privateToPublic(privateKey);
+    const address = publicToAddress(publicKey);
+
+    return { privateKey, address }
+}
 
 module.exports = async (options) => {
+    if (options.generatePrivateKey) {
+        const keyPair = generateKeyPiar();
+        
+        options.privateKey = keyPair.privateKey;
+
+        var address = keyPair.address;
+    }
 
     switchConfig(options);
 
     await require("./switchPort")(options.p2pProxyOpen);
+
+    if(address)
+    {
+        console.warn("!!!!!!!!!!!! please record the node address string, it will be use for authentication by other nodes");
+        console.warn(`node address: ${address.toString('hex')}`);
+    }
 
     process.exit(1);
 }
